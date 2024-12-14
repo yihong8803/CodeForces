4ef404284d28e46906db0796476dc1a36c592851
yihong8803 <yihong8803@gmail.com>
2024-12-14 19:22:47 +0800

    Modify 4A-Watermelon.cpp

--- 4A-Watermelon.cpp
+++ 4A-Watermelon.cpp
@@ -4,7 +4,11 @@ using namespace std;
 int main(){
     int w;
     cin >> w;
-    if (w%2 ==0){
+    if (w%2 ==0 && w!=2){
+        //See if the number would be even number or not
+        //ex: 10 divide can be 2 and 8, 12 divide can be 6 and 6, 14 divide can be 2 and 12
+        //ex: 4 divide can be 2 and 2, 6 divide can be 2 and 4
+        //ex: 2 divide can be 1 and 1 wrong
         cout<<"YES";
     }else{
         cout<<"NO";
