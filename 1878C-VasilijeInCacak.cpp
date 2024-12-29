1698e6995a3bfd078e0a7cbb2b53a05c270bc8bd
yihong8803 <yihong8803@gmail.com>
2024-12-29 15:15:09 +0800

    Mathematical Induction, Min & Max Sum for AP, long long, \n

---
+++ 1878C-VasilijeInCacak.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int t; //read the number of test cases
+	cin >> t;
+	while(t--){
+		long long n, x, k; //read n, x, k for each test case
+		cin >> n >> x >> k;
+		if(2*k>=x*(x+1) && 2*k<=n*(n+1)-(n-x)*(n-x+1)){ //check if k is between the minimum and maximum sum
+        //Min S=k(k+1)/2
+        //Max S=k*n − k*(k−1)/2
+			cout << "YES\n";
+		}
+		else cout << "NO\n";
+	}	
+}
